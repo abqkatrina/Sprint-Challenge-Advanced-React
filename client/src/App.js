d14956e1a9dd09bad491ebcb74fbb011cfe9c561
abqkatrina <abqkatrina@gmail.com>
2020-02-14 09:31:13 -0700

    set up components and hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Display from "./components/Display";
+import Header from "./components/Header";
 
-function App() {
+import "./App.css";
+
+ export default function App() {
+  const [playerData, setPlayerData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:5000/api/players"
+      )
+      .then(response => setPlayerData(response.data))
+      .catch(error => console.log( 'axios didn\'t get', error));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Display playerData={playerData} />
     </div>
   );
 }
 
-export default App;
